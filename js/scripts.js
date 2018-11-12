3b09141db9a07a50a9968a6810e596e7f7ed75b8
keitmark <keitmark@mail.ru>
2018-11-12 15:20:13 +0300

    save

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 
 $(document).ready(function () {
-    
+    $('.sec-nav__item').click(function (e) {
+        e.preventDefault;
+        var offsetPx = 0;
+        if ($(this).attr('data-href')) {
+            var el = $(this).attr('data-href');
+            console.log(el);
+            if(el == "#about"){
+                offsetPx = 60;
+            }
+            if(el == "#faq"){
+                offsetPx = -120;
+            }
+            $('body, html').animate({
+                scrollTop: $(el).offset().top - offsetPx
+            }, 700);
+            return false;
+        }
+    });
  
     
 });
@@ -8,7 +25,21 @@ $(document).ready(function () {
 
 
 $(window).load(function() {
- 
+  var win = $(window);
+    var scrFunc = function () {
+        var t = win.scrollTop(),
+            e = win.height();
+        $("[data-anim], .sec-nav, .second-nav, .sec-text-style, .score__img, .form-follow").each(function (n, i) {
+            var r = $(i).offset().top,
+                s = t + .9 * e;
+            s > r ? $(i).attr("data-anim", "true") : true;
+        });
+    }
+    scrFunc();
+    $(window).scroll(function(){
+         scrFunc();
+        
+    });
     
 });
 
